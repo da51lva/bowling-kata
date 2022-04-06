f6ba2a11234ccee71e88d5978bde8362708251ee
Matt DaSilva <matt_das@hotmail.co.uk>
2022-04-06 13:42:46 +0100

    Comments

--- src/main/java/com/techreturners/bowling_kata/BowlingGame.java
+++ src/main/java/com/techreturners/bowling_kata/BowlingGame.java
@@ -9,29 +9,35 @@ public class BowlingGame {
 
     public int play(String rollSequence) {
 
-        String[] framesAsString = rollSequence.split(" ");
+        String[] framesAsStrings = rollSequence.split(" ");
 
         int total = 0;
 
-        Frame nextFrame = mapStringToFrame(framesAsString[0]);
-        for(int i = 0; i < framesAsString.length-1; i++){
+        Frame nextFrame = mapStringToFrame(framesAsStrings[0]);
+        for(int i = 0; i < framesAsStrings.length-1; i++){ //iterate up to penultimate frame
             Frame currentFrame = nextFrame;
-            nextFrame = mapStringToFrame(framesAsString[i+1]);
+            nextFrame = mapStringToFrame(framesAsStrings[i+1]);
             currentFrame.addNextFrame(nextFrame);
             total += currentFrame.getFrameTotal();
         }
-        total += nextFrame.getFrameTotal(); //add the final frame
+        total += nextFrame.getFrameTotal(); //add the final frame to accumulator
 
 
         return (total);
     }
 
+    /**
+     * Map String representation of a frame e.g. "14" or "9/" to a Frame object
+     */
     private Frame mapStringToFrame(String frameAsString){
         Roll firstRoll = mapStringToRoll(frameAsString.substring(0,1));
         Roll secondRoll = mapStringToRoll(frameAsString.substring(1));
         return new Frame(firstRoll, secondRoll);
     }
 
+    /**
+     *  Map String representation of a roll e.g. "1" or "/" or "-" to a Roll enum
+     */
     private Roll mapStringToRoll(String rollAsString){
         Roll roll;
         switch(rollAsString) {
